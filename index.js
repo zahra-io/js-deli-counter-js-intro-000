8b648c08280b63b51f00612df10e4ea3a1415f9f
Zahra Khan <zarssskhan@gmail.com>
2017-09-12 02:53:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var katzDeli = [];
+
+// build a function that a new customer will use when entering the deli
+
+// function takeANumber should accept two paraments
+// the current line and persons name
+function takeANumber(line, name) {
+  // add a person to the line
+  line.push(name);
+  // appends a person at the end of the line if there are already peopel on it
+  // handles mulitple people on line with line.length
+  // return the position in line
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
